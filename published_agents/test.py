185b7b6e9e044d91f3a7daab3592642e955cafef
Hifaz <hifazhassan@microsoft.com>
2026-03-16 11:01:57 +0800

    Publish Agent

--- published_agents/test.py
+++ published_agents/test.py
@@ -5,11 +5,11 @@ from azure.identity import DefaultAzureCredential, get_bearer_token_provider
 # edit base_url with your <foundry-resource-name>, <project-name>, and <app-name>
 openai = OpenAI(
     api_key=get_bearer_token_provider(DefaultAzureCredential(), "https://ai.azure.com/.default"),
-    base_url="https://agents-build2026-resource.services.ai.azure.com/api/projects/agents-build2026/applications/MyAgent/protocols/openai",
+    base_url="https://ms-foundry-hif-2026.services.ai.azure.com/api/projects/foundry-hif-2026/applications/MySearchAgent/protocols/openai",
     default_query = {"api-version": "2025-11-15-preview"}
 )
 
 response = openai.responses.create( 
-  input="Write a haiku", 
+  input="Summarize blackrock fund asset", 
 ) 
 print(f"Response output: {response.output_text}")
